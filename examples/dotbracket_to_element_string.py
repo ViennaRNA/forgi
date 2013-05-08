d3ffb1bc91c612b1ef46b991d5dcbe4c12fe31df
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2013-05-08 11:08:40 +0200

    Added the dotbracket_to_element_string.py script.

---
+++ examples/dotbracket_to_element_string.py
@@ -0,0 +1,45 @@
+import sys
+from optparse import OptionParser
+
+import corgy.graph.bulge_graph as cgb
+
+def main():
+
+    usage = """
+        Usage: ./dotbracket_to_element_string.py file.dotbracket"
+
+        Convert a dotbracket notation to an element-type notation
+        where the type of each element is shown.
+
+        For example, the string '..((..))..' will be converted
+        to 'ffsshhsstt' to indicate the presence of a fiveprime,
+        stem, hairpin, and threeprime section.
+        """
+
+    parser = OptionParser(usage = usage)
+    parser.add_option('-s', '--secondary-structure', dest='sec_struct', default=False, help="Display the dotbracket represenation of the secondary structure along with the element string.", action='store_true')
+
+    (options, args) = parser.parse_args()
+
+    if len(args) < 1:
+        parser.print_help()
+        sys.exit(1)
+
+    if args[0] == '-':
+        f = sys.stdin
+    else:
+        f = open(args[0])
+
+    brackets = "".join(f.readlines()).replace('\n', '')
+    bg = cgb.BulgeGraph()
+
+
+    bg.from_dotbracket(brackets)
+
+    if options.sec_struct:
+        print bg.dotbracket_str
+
+    print bg.to_element_string()
+
+if __name__ == "__main__":
+    main()
