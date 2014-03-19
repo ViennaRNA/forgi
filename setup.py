b64eafb9992333982b26920f8049f812d57bf6c0
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2014-03-19 19:24:54 +0100

    Type in the setup.py script

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='forgi',
       author='Peter Kerpedjiev',
       author_email='pkerp@tbi.univie.ac.at',
       url='http://www.tbi.univie.ac.at/~pkerp/forgi/',
-      packages=['forgi', 'forgi.graph, forgi.utilities'],
+      packages=['forgi', 'forgi.graph', 'forgi.utilities'],
       package_data={'forgi': ['data/*.pdb']}
      )
