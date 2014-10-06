d289b105166a3acffc8e2cb1b8e4f45a145d5536
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2014-10-06 20:18:25 +0200

    Added cg_to_ss_fasta.py

---
+++ examples/cg_to_ss_fasta.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import forgi.threedee.model.coarse_grain as ftmc
+import sys
+from optparse import OptionParser
+
+def main():
+    usage = """
+    python cg_to_ss_fasta.py file.cg
+
+    Convert a coarse-grain RNA file to a dotbracket string.
+    BEWARE: Structures with pseudoknots will give bogus output.
+    """
+    num_args= 0
+    parser = OptionParser(usage=usage)
+
+    #parser.add_option('-o', '--options', dest='some_option', default='yo', help="Place holder for a real option", type='str')
+    #parser.add_option('-u', '--useless', dest='uselesss', default=False, action='store_true', help='Another useless option')
+
+    (options, args) = parser.parse_args()
+
+    if len(args) < num_args:
+        parser.print_help()
+        sys.exit(1)
+
+    cg = ftmc.CoarseGrainRNA(args[0])
+    print cg.to_fasta_string()
+
+if __name__ == '__main__':
+    main()
+
