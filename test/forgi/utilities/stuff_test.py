9071cf72151741937a71437ce1d332582e3ef2dc
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2014-10-09 20:38:46 +0200

    More efficient pair table tests

--- test/forgi/utilities/stuff_test.py
+++ test/forgi/utilities/stuff_test.py
@@ -2,6 +2,19 @@ import unittest, os
 
 class TestBulgeGraph(unittest.TestCase, GraphVerification):
     def setUp(self):
+        """
+([)]
+1234
+
+([{)]}
+123456
+
+([)]()
+123456
+"""
+        self.pt_dbs = [([7,3,4,1,2], "([)]"),
+                       ([11,4,5,6,1,2,3], "([{)]}"),
+                       ([6,3,4,1,2,6,5], "([)]()")]
         pass
 
     def test_pairtable_to_dotbracket(self):
@@ -9,23 +22,12 @@ class TestBulgeGraph(unittest.TestCase, GraphVerification):
         Convert a pair table to a dotbracket string.
 
         """
-        ot = [7,3,4,1,2]
-        db = "([)]"
-
-        self.assertEqual(fus.pairtable_to_dotbracket_string(ot), db)
-
-        db = "([{)]}"
-        ot = [11,4,5,6,1,2,3]
-
-        self.assertEqual(fus.pairtable_to_dotbracket_string(ot), db)
+        for pt,db in self.pt_dbs:
+            self.assertEqual(fus.pairtable_to_dotbracket_string(pt), db)
 
     def test_dotbracket_to_pairtable(self):
-        ot = [7,3,4,1,2]
-        db = "([)]"
-
-        self.assertEqual(fus.dotbracket_to_pairtable(db), ot)
-        
-        db = "([{)]}"
-        ot = [11,4,5,6,1,2,3]
-
-        self.assertEqual(fus.dotbracket_to_pairtable(db), ot)
+        """
+        Convert a dotbracket string to a pair table.
+        """
+        for pt,db in self.pt_dbs:
+            self.assertEqual(fus.dotbracket_to_pairtable(db), pt)
