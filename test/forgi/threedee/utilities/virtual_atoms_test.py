47cc4445b41d4f1c618a163d0b83a38570dc33e2
Bernhard Thiel <thiel@tbi.univie.ac.at>
2016-06-09 17:51:41 +0200

    Fixed test for virtual atom position.\n This code should be used as template to update fess.scripts.virtual_atom_real_atom_distances.py

--- test/forgi/threedee/utilities/virtual_atoms_test.py
+++ test/forgi/threedee/utilities/virtual_atoms_test.py
@@ -2,27 +2,38 @@ from __future__ import print_function
 import unittest
 import forgi.threedee.model.coarse_grain as ftmc
 import forgi.threedee.utilities.rmsd as ftur
+import forgi.threedee.utilities.pdb as ftup
 import Bio.PDB as bp
 import warnings
+import itertools as it
 import numpy as np
 import sys
 
-def realatom_vatom_rmsd(cg, chain):
+def realatom_vatom_rmsd(cg):
     """
     The RMSD between the real atoms and the virtual atoms of the stems.
     """
     vposs=[]
     rposs=[]
+    #print(cg.seq)
+    #print("".join(r.get_segid() for r in chain.get_residues()))
+    #print(cg.seq[1], chain[2])
     for stem in cg.stem_iterator():
         stemv=[]
         stemr=[]
-        for pos in cg.define_residue_num_iterator(stem):
-            vas=cg.virtual_atoms(pos)
+        resseqs=cg.get_resseqs(stem)
+        resseqs=resseqs[0]+resseqs[1]
+        for posCg, posPdb in zip(cg.define_residue_num_iterator(stem), resseqs):
+            #try:
+            #    print( posCg, posPdb, cg.seq[posCg-1], cg.chain[posPdb] )
+            #except KeyError as e: print(e)
+            vas=cg.virtual_atoms(posCg)
             for a, coords in vas.items():
                 try:
-                    rposs.append(chain[pos][a].get_vector().get_array())
-                    stemr.append(chain[pos][a].get_vector().get_array())
-                except KeyError: pass
+                    rposs.append(cg.chain[posPdb][a].get_vector().get_array())
+                    stemr.append(cg.chain[posPdb][a].get_vector().get_array())
+                except KeyError: 
+                    pass
                 else:
                     vposs.append(coords)
                     stemv.append(coords)
@@ -35,48 +46,26 @@ def realatom_vatom_rmsd(cg, chain):
 class VirtualAtomsTest(unittest.TestCase):
     def setUp(self):
         pdbfile1 = "test/forgi/threedee/data/3FU2.pdb"
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.pdb1 = list(bp.PDBParser().get_structure('test', pdbfile1).get_chains())[0]
         self.cg1  = ftmc.from_pdb(pdbfile1)
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.pdb1b = list(bp.PDBParser().get_structure('test', pdbfile1).get_chains())[0]
-        self.cg1b  = ftmc.from_pdb(pdbfile1)
-        #pdbfile2 = "test/forgi/threedee/data/3V2F.pdb"  #Takes some time. Big structure
-        #with warnings.catch_warnings():
-        #    warnings.simplefilter("ignore")
-        #    self.pdb2 = list(bp.PDBParser().get_structure('test', pdbfile2).get_chains())[0]
-        #self.cg2  = ftmc.from_pdb(pdbfile2)
+
+        pdbfile2 = "test/forgi/threedee/data/3V2F.pdb"  #Takes some time. Big structure
+        self.cg2  = ftmc.from_pdb(pdbfile2)
 
         pdbfile3 = "test/forgi/threedee/data/1X8W.pdb"
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.pdb3 = list(bp.PDBParser().get_structure('test', pdbfile3).get_chains())[0]
         self.cg3  = ftmc.from_pdb(pdbfile3)
 
         pdbfile4 = "test/forgi/threedee/data/2QBZ.pdb" 
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.pdb4 = list(bp.PDBParser().get_structure('test', pdbfile4).get_chains())[0]
         self.cg4  = ftmc.from_pdb(pdbfile4)
 
     def test_virtualatoms_close_to_pdb(self):
-        self.assertLess(realatom_vatom_rmsd(self.cg4, self.pdb4), 32) #31.97
-        self.assertLess(realatom_vatom_rmsd(self.cg3, self.pdb3), 29.3) #22.23
-        #self.assertLess(realatom_vatom_rmsd(self.cg2, self.pdb2), 60)
-        self.assertLess(realatom_vatom_rmsd(self.cg1, self.pdb1), 2.39) #2.21 without virtual residues
-        #The difference between average_virtual_atoms and stem-based virtual atoms is negelctable.
-    def test_va_rmsd_always_the_same(self):
-        r1=realatom_vatom_rmsd(self.cg1, self.pdb1)
-        r2=realatom_vatom_rmsd(self.cg1, self.pdb1)
-        self.assertEqual(r1,r2)
-        r1=realatom_vatom_rmsd(self.cg1, self.pdb1)
-        r2=realatom_vatom_rmsd(self.cg1b, self.pdb1b)
-        self.assertEqual(r1,r2)
-        r1=realatom_vatom_rmsd(self.cg1, self.pdb1b)
-        r2=realatom_vatom_rmsd(self.cg1b, self.pdb1)
-        self.assertEqual(r1,r2)
-        r1=realatom_vatom_rmsd(self.cg1, self.pdb1)
-        r2=realatom_vatom_rmsd(self.cg1b, self.pdb1)
-        self.assertEqual(r1,r2)
+        #print(realatom_vatom_rmsd(self.cg1), file=sys.stderr)
+        #print(realatom_vatom_rmsd(self.cg2), file=sys.stderr)
+        #print(realatom_vatom_rmsd(self.cg3), file=sys.stderr)
+        #print(realatom_vatom_rmsd(self.cg4), file=sys.stderr)
+        self.assertLess(realatom_vatom_rmsd(self.cg1), 1.16) # 1.32 using average atoms (no vres)
+        self.assertLess(realatom_vatom_rmsd(self.cg3), 1.36) # 1.33
+        self.assertLess(realatom_vatom_rmsd(self.cg4), 1.32) # 1.16
+        self.assertLess(realatom_vatom_rmsd(self.cg2), 1.42) # 1.30
+
+
+
