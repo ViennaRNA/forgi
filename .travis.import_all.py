2925cf168ce8db9d9d6f2b2e89099c83ed406e27
Bernhard Thiel <thiel@tbi.univie.ac.at>
2017-06-30 11:45:40 +0200

    On travis, nose2 is killed before it produces any output. I imagin it could be due to excessive memory consumpiton during  byte-compling the forgi repo. To verify this, I add a script that only imports all of forgi.

---
+++ .travis.import_all.py
@@ -0,0 +1,16 @@
+import forgi.threedee.model.Element
+import forgi.graph.bulge_graph
+import forgi.projection.hausdorff
+import forgi.projection.projection2d
+import forgi.threedee.model.coarse_grain
+import forgi.threedee.model.descriptors
+import forgi.threedee.model.similarity 
+import forgi.threedee.model.stats 
+import forgi.threedee.utilities.average_atom_positions
+import forgi.threedee.utilities.dssr 
+import forgi.threedee.utilities.graph_pdb 
+import forgi.threedee.utilities.pdb 
+import forgi.threedee.utilities.vector 
+import forgi.utilities.debug as fud
+import forgi.utilities.graph as fug
+import forgi.utilities.stuff as fus
