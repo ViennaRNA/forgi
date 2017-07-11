7feef214de83f443cb1670eae4ba7fe6bcc7a9df
Bernhard C. Thiel <thiel@tbi.univie.ac.at>
2017-07-11 22:52:16 +0200

    Added -k option to descripe_cg.py. This prevents showing some descriptors. In the future, it should also prevent CALCULATING unnecessary descriptors.

--- examples/describe_cg.py
+++ examples/describe_cg.py
@@ -2,22 +2,25 @@
 from __future__ import print_function, absolute_import, division, unicode_literals
 from builtins import (ascii, bytes, chr, dict, filter, hex, input,
                       map, next, oct, open, pow, range, round,
-                      str, super, zip) 
+                      str, super, zip)
 
 
 
 import argparse
 from collections import defaultdict
+import forgi.utilities.commandline_utils as fuc
 import forgi.threedee.model.descriptors as ftmd
 import forgi.threedee.model.coarse_grain as ftmc
 import pandas as pd
 import logging
 log = logging.getLogger(__name__)
+
 def generateParser():
-    parser=argparse.ArgumentParser( description="Collect data about a list of pdb files and store it as a csv.")
-    parser.add_argument("files", type=str, nargs="+", help="One or more cg files.")
-    parser.add_argument("--verbose", "-v", action="store_true", help="Be verbose.")
+    parser=fuc.get_rna_input_parser("Collect data about a list of rna files and store it as a csv.",
+                                     nargs="+", rna_type="any", enable_logging=True)
     parser.add_argument("--csv", type=str, help="Store dataframe under this filename. (Prints to stdout if not given)")
+    parser.add_argument("-k", "--keys", type=str, help="Only print the following properties. "
+                                        "(A comma-seperated list of column headers, e.g. rog_vres")
 
     return parser
 
@@ -25,14 +28,10 @@ def generateParser():
 parser = generateParser()
 if __name__=="__main__":
     args = parser.parse_args()
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
+    cgs = fuc.cgs_from_args(args, "+", "any", enable_logging=True)
     data = defaultdict(list)
-    for i, filename in enumerate(args.files):
-        cg = ftmc.CoarseGrainRNA(filename)
-        log.debug("File {} read".format(cg.name))
-        data["pdb_name"].append(cg.name)
-        data["path"].append(filename)
+    for i, cg in enumerate(cgs):
+        data["name"].append(cg.name)
         data["nt_length"].append(cg.seq_length)
         data["num_cg_elems"].append(len(cg.defines))
         for letter in "smifth":
@@ -66,6 +65,11 @@ if __name__=="__main__":
             data["anisotropy_vres"].append(ftmd.anisotropy(cg.get_ordered_virtual_residue_poss()))
             data["asphericity_fast"].append(ftmd.asphericity(cg.get_ordered_stem_poss()))
             data["asphericity_vres"].append(ftmd.asphericity(cg.get_ordered_virtual_residue_poss()))
+    if args.keys:
+        allowed_keys = args.keys.split(",")
+        for key in list(data.keys()):
+            if key not in allowed_keys:
+                del data[key]
     df = pd.DataFrame(data)
     if args.csv:
         df.to_csv(args.csv, mode='a')
