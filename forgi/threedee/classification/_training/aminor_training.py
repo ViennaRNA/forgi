7752889c7583eee667a0290354ade4fad1eee843
Bernhard Thiel <thiel@tbi.univie.ac.at>
2018-07-05 13:17:41 +0200

    separate training script and training module

--- forgi/threedee/classification/_training/aminor_training.py
+++ forgi/threedee/classification/_training/aminor_training.py
@@ -19,7 +19,6 @@ from sklearn.metrics import confusion_matrix, f1_score
 import pandas as pd
 
 from .. import aminor as ftca
-import forgi.utilities.commandline_utils as fuc
 import forgi.utilities.stuff as fus
 from ._parse_utils import ChainIdMappingParser
 import forgi.graph.residue as fgr
@@ -34,45 +33,6 @@ PDBIDS_SRSU=["1G1X", "1I6U", "1KUQ", "2VQE", "4V19", "4v9o", "5lzd", "5mmm",
              "5o61", "5OOL", "5v93","3J7A", "3J7P", "4UG0", "3JAM", "4P8Z",
              "4UG0", "4V5O", "4V88", "5FLX", "5LZS","5OPT", "5XXU", "5XYI", "6AZ1"]
 
-################################################################################
-### Lets use this as a script
-### Run as python -m forgi.threedee.classification._training.aminor_training
-################################################################################
-def main():
-    parser = fuc.get_rna_input_parser("Train a classifier for A-Minior interactions.",
-                                      nargs="+", rna_type="only_cg")
-    parser.add_argument("--fr3d-result", type=str, required=True,
-                        help="A file containing the FR3D output")
-    parser.add_argument("--fr3d-query", type=str,
-                        help="Add this string describing the FR3D query "
-                             "as a comment to the trainingsdata-out file")
-    parser.add_argument("--chain-id-mapping-dir", type=str,
-                        help="If you use PDB-bundles, this directory "
-                             "needs to hold all chain-id-mapping.txt files.")
-    parser.add_argument("--trainingsdata-out", type=str,
-                        default="forgi/threedee/data/aminor_geometries.csv",
-                        help="File that will be written for the geometries "
-                             "of interactions and non-interactions.")
-    parser.add_argument("--model-params-out", type=str,
-                        default="forgi/threedee/data/aminor_params.json",
-                        help="File that will be written for the "
-                             "model's hyper-parameters.")
-    parser.add_argument("--test-set", type=str, help="':'-separated PDB-ids"
-                                                     " for the test-set.")
-    parser.add_argument("--train-set", type=str,
-                        help="':'-separated PDB-ids for the train-set."
-                             "Note: This is only used for cross-validation."
-                             "The final model will be trained on all the data.")
-
-    args = parser.parse_args()
-    cgs, cg_filenames = fuc.cgs_from_args(args, "+", rna_type="only_cg",
-                                          return_filenames=True)
-    create_geometry_file(args.trainingsdata_out, cgs, cg_filenames,
-                         args.fr3d_result, args.chain_id_mapping_dir,
-                         args.fr3d_query)
-    hyper_params = tune_model(args.trainingsdata_out, args.train_set, args.test_set)
-    with open("forgi/threedee/data/aminor_params.json", "w") as f:
-        json.dump(hyper_params,f)
 
 def create_geometry_file(outfile, cgs, cg_filenames, fr3d_result,
                          chain_id_mapping_dir, fr3d_query=""):
@@ -468,6 +428,3 @@ def _enumerate_background_geometries(all_cgs, cutoff_dist, aminor_geometries):
                             non_ame_geometries.add(geometry)
     log.error("%s non_ame geometries found", len(non_ame_geometries))
     return non_ame_geometries
-
-if __name__ == "__main__":
-    main()
