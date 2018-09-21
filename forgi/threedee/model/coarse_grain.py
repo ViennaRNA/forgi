ViennaRNA/forgi

commit 0fbf18c3785b2f39f3152952fbe8121d65bcc753

Update dssr module to parse negative residue numbers

forgi/threedee/utilities/_dssr.py
@@ -29,19 +29,16 @@ def dssr_to_pdb_resid(dssr_resid):
         resid = dssr_resid
     if "^" in resid:
         resid, _, letter = resid.partition("^")
-        nat = []
-        for c in reversed(resid):
-            if c.isdigit(): nat.append(c)
-            else: break
-        resid="".join(reversed(nat))
-        return RESID(chain, (" ", int(resid) , letter))
     else:
-        nat = []
-        for c in reversed(resid):
-            if c.isdigit(): nat.append(c)
-            else: break
-        resid="".join(reversed(nat))
-        return RESID(chain, (" ", int(resid) ," "))
+        letter=" "
+    nat = []
+    for c in reversed(resid):
+        if c.isdigit() or c=="-":
+            nat.append(c)
+        else:
+            break
+    resid="".join(reversed(nat))
+    return RESID(chain, (" ", int(resid) ," "))
 
 
 class DSSRAnnotation(object):

forgi/graph/sequence.py
@@ -242,7 +242,7 @@ class SeqidList(Sequence):
             c = Counter(self._list)
             for k,amount in c.most_common():
                 if amount>1:
-                    log.error("Seq_id %s (%s)  occurs %s times!", k, repr(k), amount)
+                    log.error("Seq_id %s  occurs %s times!", k, amount)
                 else:
                     break
             raise ValueError("Duplicate Seq_id encountered: {}".format(k))

test/forgi/threedee/utilities/test_dssr.py
@@ -11,6 +11,8 @@ class TestHelperFunctions(unittest.TestCase):
         self.assertEqual(ftud.dssr_to_pdb_resid("C.C47^M"), ("C", (" ", 47, "M")))
         self.assertEqual(ftud.dssr_to_pdb_resid("C.5BU47^M"), ("C",(" ", 47, "M")))
         self.assertEqual(ftud.dssr_to_pdb_resid(u'A.C1'), ("A",(" ", 1, " ")))
+        self.assertEqual(ftud.dssr_to_pdb_resid(u'B.U-1'), ("B",(" ", -1, " ")))
+        self.assertEqual(ftud.dssr_to_pdb_resid(u'A.A-2'), ("A",(" ", -2, " ")))
 
 @unittest.skip("DSSR module not yet ported to multiple chain cg model.")
 class TestCoaxialStacks(unittest.TestCase):

commit 0c4499f0586c09837e31ff4e26d06fd0eaf92615

Fix a Bug introduced in 0fbf18c3785b2f39f3152952fbe8121d65bcc753

forgi/threedee/model/coarse_grain.py
@@ -1443,6 +1443,8 @@ class CoarseGrainRNA(fgb.BulgeGraph):
             return self.twists[node]
 
         connections = list(self.edges[node])
+        if not connections:
+            log.error("%s has no edges in cg with defines %s and edges %s", node, self.defines, self.edges)
         (s1b, s1e) = self.get_sides(connections[0], node)
 
         if len(connections) == 1:

forgi/threedee/utilities/_dssr.py
@@ -38,7 +38,7 @@ def dssr_to_pdb_resid(dssr_resid):
         else:
             break
     resid="".join(reversed(nat))
-    return RESID(chain, (" ", int(resid) ," "))
+    return RESID(chain, (" ", int(resid) ,letter))
 
 
 class DSSRAnnotation(object):