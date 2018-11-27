82b740e9e01cd7589ac47a164c7236898379b2cf
Bernhard Thiel <thiel@tbi.univie.ac.at>
2018-11-27 14:28:33 +0100

    Try to make setup.py work with windows.

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup, Extension
 from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 import subprocess
 import os
 import itertools
 import warnings
 import numpy
+import logging
 
+logging.basicConfig()
+log = logging.getLogger(__file__)
+
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError, IOError)
 
 def try_cythonize(arg):
   try:
     from Cython.Build import cythonize
     return cythonize([Extension("", [arg+".pyx"], include_dirs=[numpy.get_include()])])
   except Exception as e:
-    try:
-        return [Extension("", [arg+".c"], include_dirs=[numpy.get_include()])]
-    except Exception as e:
-        print("Could not compile C-code. Falling back to pure-python code")
-        return []
+    return [Extension("", [arg+".c"], include_dirs=[numpy.get_include()])]
 
 try: #If we are in a git-repo, get git-describe version.
     path = os.path.abspath(os.path.dirname(__file__))
@@ -45,6 +48,28 @@ try: #If we are in a git-repo, get git-describe version.
 except: #Outside of a git repo, do nothing.
     build_py = _build_py
 
+class BuildFailed(Exception):
+    pass
+
+def construct_build_ext(build_ext):
+    """
+    Thanks to https://stackoverflow.com/a/41785785/5069869 and https://github.com/Toblerity/Shapely/blob/master/setup.py
+    """
+    class WrappedBuildExt(build_ext):
+        # This class allows C extension building to fail.
+        def run(self):
+            try:
+                build_ext.run(self)
+            except DistutilsPlatformError as x:
+                raise BuildFailed(x)
+
+        def build_extension(self, ext):
+            try:
+                build_ext.build_extension(self, ext)
+            except ext_errors as x:
+                raise BuildFailed(x)
+    return WrappedBuildExt
+
 
 extras = {"forgi.visual":["matplotlib>=2.0"],
           "development":["cython"],
@@ -52,32 +77,31 @@ extras = {"forgi.visual":["matplotlib>=2.0"],
           "pdbechem":['beautifulsoup4>=4.6']
          }
 extras["all"]=list(itertools.chain(extras.values()))
-
-setup(
-      zip_safe=False,
-      cmdclass={'build_py': build_py},
-      name='forgi',
-      version='2.0.1-alpha',
-      description='RNA Graph Library',
-      author='Peter Kerpedjiev, Bernhard Thiel',
-      author_email='pkerp@tbi.univie.ac.at, thiel@tbi.univie.ac.at',
-      license='GNU Affero GPL 3.0',
-      url='http://www.tbi.univie.ac.at/~pkerp/forgi/',
-      ext_modules = try_cythonize("forgi/threedee/utilities/cytvec"), 
-      packages=['forgi', 'forgi.graph', 'forgi.threedee',
+setup_args = {
+      "zip_safe":False,
+      "cmdclass":{'build_py': build_py, 'build_ext':construct_build_ext(build_ext)},
+      "name":'forgi',
+      "version":'2.0.1-alpha',
+      "description":'RNA Graph Library',
+      "author":'Bernhard Thiel, Peter Kerpedjiev',
+      "author_email":'thiel@tbi.univie.ac.at',
+      "license":'GNU Affero GPL 3.0',
+      "url":'http://www.tbi.univie.ac.at/~pkerp/forgi/',
+      "ext_modules": try_cythonize("forgi/threedee/utilities/cytvec"), 
+      "packages":['forgi', 'forgi.graph', 'forgi.threedee',
                 'forgi.threedee.model', 'forgi.utilities',
                 'forgi.threedee.utilities',
                 'forgi.threedee.classification',
                 'forgi._k2n_standalone', 'forgi.threedee.visual',
                 'forgi.visual', 'forgi.projection'],
-      package_data={'forgi.threedee': ['data/*.pdb', 'data/stats/temp.stats', 'data/average_atom_positions.json', 'data/aminor_geometries.csv', 'data/aminor_params.json']},
-      scripts=['examples/rnaConvert.py',
+      "package_data":{'forgi.threedee': ['data/*.pdb', 'data/stats/temp.stats', 'data/average_atom_positions.json', 'data/aminor_geometries.csv', 'data/aminor_params.json']},
+      "scripts":['examples/rnaConvert.py',
                'examples/describe_cg.py',
                'examples/compare_RNA.py',
                'examples/visualize_rna.py',
                'examples/pseudoknot_analyzer.py',
                'examples/forgi_config.py'],
-      install_requires=[
+      "install_requires":[
 		'numpy>=1.10.0',
                 'scipy>=0.19.1',
                 'networkx>=2.0',
@@ -87,14 +111,14 @@ setup(
                 'appdirs>=1.4',
                 'logging_exceptions>=0.1.8',
 	],
-      extras_require=extras,
+      "extras_require":extras,
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
+    "classifiers":[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
@@ -113,5 +137,13 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
          ],
+     }
 
-     )
+try:
+    setup(**setup_args)
+except BuildFailed as ex:
+    log.warning("The C extension could not be compiled")
+    del setup_args[ext_modules]
+    setup(**setup_args)
+    log.info("Installed forgi without faster compiled C-code.")
+
