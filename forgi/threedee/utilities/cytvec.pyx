4cdc630f3a274ae1ddb02aa7daaf2ee641f8e573
Bernhard Thiel <thiel@tbi.univie.ac.at>
2019-03-07 14:41:57 +0100

    Update code for broken_ml

--- forgi/threedee/utilities/cytvec.pyx
+++ forgi/threedee/utilities/cytvec.pyx
@@ -4,22 +4,43 @@ cimport cython
 cimport numpy as np
 from libc.math cimport sqrt, cos, sin, M_PI, acos
 
-#ctypedef np.ndarray[double, ndim=1] np.ndarray[double, ndim=1]
-#ctypedef np.ndarray[double, ndim=2] np.ndarray[double, ndim=2]
-
+@cython.boundscheck(False)
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
-cdef double magnitude(np.ndarray[double, ndim=1] vec) except -1:
+cdef double magnitude(double [:] vec) except -1:
     return sqrt(vec[0]*vec[0]+vec[1]*vec[1]+vec[2]*vec[2])
 
+
+@cython.boundscheck(False)
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 cpdef np.ndarray[double, ndim=2] create_orthonormal_basis(
-                        np.ndarray[double, ndim=1] vec1,
-                        np.ndarray[double, ndim=1] vec2):
-    vec1/=magnitude(vec1)
-    vec2/=magnitude(vec2)
-    cdef np.ndarray[double, ndim=1] vec3 = np.array([vec1[1]*vec2[2]-vec1[2]*vec2[1],  vec1[2]*vec2[0]-vec1[0]*vec2[2], vec1[0]*vec2[1]-vec1[1]*vec2[0] ])
-    return np.array([vec1, vec2, vec3])
+                             double[:] vec1,
+                             double[:] vec2): # Optimized
+    cdef double ma, mb, mc
+    cdef double ax,ay,az,bx,by,bz
+    ma =magnitude(vec1)
+    mb =magnitude(vec2)
+    ax=vec1[0]/ma
+    ay=vec1[1]/ma
+    az=vec1[2]/ma
+    bx=vec2[0]/mb
+    by=vec2[1]/mb
+    bz=vec2[2]/mb
 
+    cdef np.ndarray[double, ndim=2] out = np.zeros((3,3))
+    out[0,0]=ax
+    out[0,1]=ay
+    out[0,2]=az
+    out[1,0]=bx
+    out[1,1]=by
+    out[1,2]=bz
+    out[2,0]=ay*bz-az*by
+    out[2,1]=az*bx-ax*bz
+    out[2,2]=ax*by-ay*bx
+
+    return out
+
+
+@cython.boundscheck(False)
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 cdef np.ndarray[double, ndim=1] transform_coords(np.ndarray[double, ndim=2] transposed_stem1_basis,
                            float r, float u, float v):
@@ -41,6 +62,7 @@ cdef np.ndarray[double, ndim=2] rotation_matrix(char axis, double theta):
     else:
         assert False
 
+@cython.boundscheck(False)
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 cdef np.ndarray[double, ndim=1] twist2_orient_from_stem1_1(np.ndarray[double, ndim=2] stem1_basis, double u, double v, double t):
     cdef np.ndarray[double, ndim=1] twist2_new = np.array([0., cos(t), sin(t)])
@@ -56,7 +78,7 @@ cdef np.ndarray[double, ndim=1] twist2_orient_from_stem1_1(np.ndarray[double, nd
 
     return twist2_new_basis
 
-
+@cython.boundscheck(False)
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 cdef np.ndarray[double, ndim=1] spherical_polar_to_cartesian(double r, double u, double v):
     cdef double x=r*sin(u)*cos(v)
@@ -65,8 +87,17 @@ cdef np.ndarray[double, ndim=1] spherical_polar_to_cartesian(double r, double u,
     return np.array([x,y,z])
 
 
+def get_fixed_stemvec(cg, broken_ml_name, fixed_stem_name):
+    sides = cg.get_sides(fixed_stem_name, broken_ml_name)
+    cdef np.ndarray[double, ndim=1] fixed_s_vec = cg.coords.get_direction(fixed_stem_name)
+    if sides[0]==0:
+        fixed_s_vec = -fixed_s_vec
+    cdef np.ndarray[double, ndim=1] fixed_s_pos = cg.coords[fixed_stem_name][sides[0]]
+    cdef np.ndarray[double, ndim=1] s_twist = cg.twists[fixed_stem_name][sides[0]]
 
-def get_broken_ml_deviation(cg, broken_ml_name, fixed_stem_name, virtual_stat):
+    return fixed_s_pos, fixed_s_vec, s_twist
+
+def get_orig_coords(cg, broken_ml_name, fixed_stem_name):
     s1, s2 = cg.edges[broken_ml_name]
     if s1 == fixed_stem_name:
         orig_stem_name = s2
@@ -75,45 +106,68 @@ def get_broken_ml_deviation(cg, broken_ml_name, fixed_stem_name, virtual_stat):
     else:
         raise ValueError("fixed stem {} is not attached to ml {} with "
                          "edges {}".format(fixed_stem_name, broken_ml_name, [s1,s2]))
-
-    sides = cg.get_sides(fixed_stem_name, broken_ml_name)
-    cdef np.ndarray[double, ndim=1] fixed_s_vec = cg.coords.get_direction(fixed_stem_name)
-    if sides[0]==0:
-        fixed_s_vec = -fixed_s_vec
-    cdef np.ndarray[double, ndim=1] s_twist = cg.twists[fixed_stem_name][sides[0]]
-    cdef np.ndarray[double, ndim=2] fixed_stem_basis = create_orthonormal_basis(fixed_s_vec, s_twist)
-
-    cdef np.ndarray[double, ndim=2] transposed_stem1_basis = fixed_stem_basis.transpose()
-
-    cdef np.ndarray[double, ndim=1] vbulge_vec = transform_coords(transposed_stem1_basis,
-                                                  virtual_stat.r1,
-                                                  virtual_stat.u1,
-                                                  virtual_stat.v1)
-    cdef np.ndarray[double, ndim=1] vstem_vec = transform_coords(transposed_stem1_basis,
-                                                  1.,
-                                                  virtual_stat.u,
-                                                  virtual_stat.v)
-    cdef np.ndarray[double, ndim=1] vstem_twist = twist2_orient_from_stem1_1(transposed_stem1_basis,
-                                                   virtual_stat.u,
-                                                   virtual_stat.v,
-                                                   virtual_stat.t)
-
-    vstem_vec *=5
-    cdef np.ndarray[double, ndim=1] vstem_coords0 = cg.coords[fixed_stem_name][sides[0]] + vbulge_vec
-    cdef np.ndarray[double, ndim=1] vstem_coords1 = vstem_coords0 + vstem_vec
-
     sides2 = cg.get_sides(orig_stem_name, broken_ml_name)
     cdef np.ndarray[double, ndim=1] orig_coords0 = cg.coords[orig_stem_name][sides2[0]]
     cdef np.ndarray[double, ndim=1] orig_coords1 = cg.coords[orig_stem_name][sides2[1]]
+    cdef np.ndarray[double, ndim=1] orig_twist   =  cg.twists[orig_stem_name][sides2[0]]
+    return orig_coords0, orig_coords1, orig_twist
+
+def get_broken_ml_deviation(cg, broken_ml_name, fixed_stem_name, virtual_stat):
+    cdef np.ndarray[double, ndim=1] fixed_s_pos, fixed_s_vec, s_twist
+    cdef np.ndarray[double, ndim=1] orig_coords0, orig_coords1, orig_twist
+
+    fixed_s_pos, fixed_s_vec, s_twist = get_fixed_stemvec(cg, broken_ml_name, fixed_stem_name)
+    orig_coords0, orig_coords1, orig_twist = get_orig_coords(cg, broken_ml_name, fixed_stem_name)
+
+    cdef np.ndarray[double, ndim=1] v_stat = np.asarray([virtual_stat.r1,
+                                                             virtual_stat.u1,
+                                                             virtual_stat.v1,
+                                                             virtual_stat.u,
+                                                             virtual_stat.v,
+                                                             virtual_stat.t])
+    return _get_broken_ml_dev_core(fixed_s_pos, fixed_s_vec, orig_coords0, orig_coords1, orig_twist, s_twist, v_stat)
+
+
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.wraparound(False)
+cdef _get_broken_ml_dev_core(np.ndarray[double, ndim=1] fixed_s_pos,
+                             np.ndarray[double, ndim=1] fixed_s_vec,
+                             np.ndarray[double, ndim=1] orig_coords0,
+                             np.ndarray[double, ndim=1] orig_coords1,
+                             np.ndarray[double, ndim=1] orig_twist,
+                             np.ndarray[double, ndim=1] s_twist,
+                             np.ndarray[double, ndim=1] virtual_stat):
+
+    cdef np.ndarray[double, ndim=2] fixed_stem_basis = create_orthonormal_basis(fixed_s_vec, s_twist)
+    cdef np.ndarray[double, ndim=2] transposed_stem1_basis = fixed_stem_basis.transpose()
+
+    cdef np.ndarray[double, ndim=1] vbulge_vec = transform_coords(transposed_stem1_basis,
+                                                  virtual_stat[0],
+                                                  virtual_stat[1],
+                                                  virtual_stat[2])
+    cdef np.ndarray[double, ndim=1] vstem_vec = transform_coords(transposed_stem1_basis,
+                                                  1.,
+                                                  virtual_stat[3],
+                                                  virtual_stat[4])
+    cdef np.ndarray[double, ndim=1] vstem_twist = twist2_orient_from_stem1_1(transposed_stem1_basis,
+                                                   virtual_stat[3],
+                                                   virtual_stat[4],
+                                                   virtual_stat[5])
+
+    vstem_vec *=5
+    cdef np.ndarray[double, ndim=1] vstem_coords0 = fixed_s_pos + vbulge_vec
+    cdef np.ndarray[double, ndim=1] vstem_coords1 = vstem_coords0 + vstem_vec
 
     cdef np.ndarray[double, ndim=1] orig_stem_vec = orig_coords1 - orig_coords0
-    cdef np.ndarray[double, ndim=1] true_bulge_vec = orig_coords0 - cg.coords[fixed_stem_name][sides[0]]
+    cdef np.ndarray[double, ndim=1] true_bulge_vec = orig_coords0 - fixed_s_pos
 
     cdef double pos_dev = ( vec_distance(orig_coords0, vstem_coords0) )
     cdef double ang_dev = vec_angle(vstem_vec, orig_stem_vec)
-    cdef double twist_dev = vec_angle(cg.twists[orig_stem_name][sides2[0]], vstem_twist)
+    cdef double twist_dev = vec_angle(orig_twist, vstem_twist)
     return pos_dev, ang_dev, twist_dev
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef double vec_angle(np.ndarray[double, ndim=1] vec1,
                                     np.ndarray[double, ndim=1] vec2):
         '''
@@ -142,6 +196,8 @@ cdef double vec_angle(np.ndarray[double, ndim=1] vec1,
 
         return  acos(d)
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef np.ndarray[double, ndim=1] normalize(np.ndarray[double, ndim=1] vec):
     '''
     Normalize a vector so that its magnitude becomes 1.0 while
@@ -153,6 +209,8 @@ cdef np.ndarray[double, ndim=1] normalize(np.ndarray[double, ndim=1] vec):
     cdef double mag = magnitude(vec)
     return vec / mag
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef double vec_distance( np.ndarray[double, ndim=1] vec1,
                                          np.ndarray[double, ndim=1] vec2):
     cdef np.ndarray[double, ndim=1] direction = vec2-vec1
--- forgi/threedee/utilities/cytvec_setup.py
+++ forgi/threedee/utilities/cytvec_setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from Cython.Build import cythonize
-
+import numpy
 setup(
     name='cytvec',
-    ext_modules=cythonize("cytvec.pyx"),
+    ext_modules=cythonize("cytvec.pyx"),include_dirs=[numpy.get_include()]
 )
