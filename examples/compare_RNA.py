8b7937b36109a8f4b0c2837d7fff0f96233fbdaa
Bernhard Thiel <thiel@tbi.univie.ac.at>
2019-03-22 11:10:47 +0100

    Change way we handle small differences in seq_ids or sequence.

--- examples/compare_RNA.py
+++ examples/compare_RNA.py
@@ -66,27 +66,89 @@ def pdb_rmsd(cg1, cg2):
         reslist2 = []
         chain1, = cg1.chains.values()
         chain2, = cg2.chains.values()
-        reslist1.extend(cr for cr in chain1.get_list()
-                        if cr.resname.strip() in ftup.RNA_RESIDUES)
-        reslist2.extend(cr for cr in chain2.get_list()
-                             if cr.resname.strip() in ftup.RNA_RESIDUES)
-        print("PDB-RMSD (single chain):\t{:.3f}".format(
-                  ftup.residuelist_rmsd(reslist1, reslist2, sidechains=True)[1]))
-        return True
+        resnames1=[ r.resname.strip() for r in chain1 ]
+        resnames2=[ r.resname.strip() for r in chain2 ]
+        log.info("Resname lists are %s, %s", resnames1, resnames2)
+        if resnames1 == resnames2:
+            for r in chain1:
+                if r.resname.strip() in ftup.RNA_RESIDUES:
+                    reslist1.append(r)
+            for r in chain2:
+                if r.resname.strip() in ftup.RNA_RESIDUES:
+                    reslist2.append(r)
+            print("PDB-RMSD (single chain):\t{:.3f}".format(
+                      ftup.residuelist_rmsd(reslist1, reslist2, sidechains=True)[1]))
+            return True
+
+        else:
+            log.info("Resname lists are different")
+            reslist1, reslist2 = common_reslists(chain1, chain2)
+            print("PDB-RMSD (single chain):\t{:.3f}".format(
+                      ftup.residuelist_rmsd(reslist1, reslist2, sidechains=True)[1]))
+            return True
     else:
+        log.info("Chain lengths are %s and %s",len(cg1.chains), len(cg2.chains) )
         common_chains = set(cg1.chains.keys()) & set(cg2.chains.keys())
         reslist1 = []
         reslist2 = []
         for chain in common_chains:
-            reslist1.extend(cr for cr in cg1.chains[chain].get_list()
-                             if cr.resname.strip() in ftup.RNA_RESIDUES)
-            reslist2.extend(cr for cr in cg2.chains[chain].get_list()
-                             if cr.resname.strip() in ftup.RNA_RESIDUES)
+            ext1, ext2 = common_reslists(cg1.chains[chain], cg2.chains[chain])
+            reslist1.extend(ext1)
+            reslist2.extend(ext2)
         if common_chains:
             print("PDB-RMSD (chains {}):\t{:.3f}".format("-".join(common_chains),
                   ftup.residuelist_rmsd(reslist1, reslist2, sidechains=True)[1]))
             return True
         return False
+
+
+def common_reslists(chain1, chain2):
+    reslist1 = []
+    reslist2 = []
+    start1 = chain1.get_list()[0].id[1]
+    start2 = chain2.get_list()[0].id[1]
+    offset=start2-start1
+    mismatches=float("inf")
+    for offs in [0, offset]:
+        rl1, rl2, m = _common_reslist_with_offset(chain1, chain2, offs)
+        if m<mismatches:
+            mismatches=m
+            reslist1, reslist2 = rl1, rl2
+    if mismatches:
+        log.warning("There were %s point mutations between the two chains", mismatches)
+    if mismatches>8:
+        raise ValueError("Could not find mapping between chains!")
+    return reslist1, reslist2
+
+def _common_reslist_with_offset(chain1, chain2, offset):
+    log.info("Trying mapping with offset %s", offset)
+    reslist1 = []
+    reslist2 = []
+    for cr2 in chain2.get_list():
+        mapped_id = cr2.id[0], cr2.id[1]-offset, cr2.id[2]
+        if mapped_id not in chain1:
+            log.info("Residue %s (%s) [->%s] not in chain1", cr2.id, cr2.resname.strip(), mapped_id)
+            log.info("Res %s has atoms %s", mapped_id, cr2.get_list())
+    mismatch=0
+    for cr1 in chain1.get_list():
+        mapped_id = cr1.id[0], cr1.id[1]+offset, cr1.id[2]
+        cr1_name = cr1.resname.strip()
+        try:
+            cr2 = chain2[mapped_id]
+        except KeyError as e:
+            log.info("Residue %s (%s) [->%s] not in chain2", cr1.id, cr1_name, mapped_id)
+            continue
+        cr2_name = cr2.resname.strip()
+        if cr1_name != cr2_name:
+            mismatch+=1
+            log.info("Residue %s [->%s](chains %s, %s) has different name in the "
+                        "two chains: %s!=%s", cr1.id, mapped_id, chain1.id, chain2.id, cr1_name, cr2_name)
+        if cr1_name in ftup.RNA_RESIDUES and cr2_name in ftup.RNA_RESIDUES:
+            reslist1.append(cr1)
+            reslist2.append(cr2)
+
+    return reslist1, reslist2, mismatch
+
 
 
 parser = get_parser()
